2a7168bc17ada9cecf54701a3ea838539c359bc7
unknown <lc.bonini13@gmail.com>
2022-09-11 13:12:00 -0300

    Feat: Criação do Header

---
+++ src/app/Core/Components/Header/header.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'fb-header',
+    templateURL: './header.component.html',
+    styleUrls: ['./header.component.scss'],
+})
+export class HeaderComponent {
+    
+}
